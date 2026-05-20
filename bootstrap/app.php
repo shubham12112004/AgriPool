e3dbd95962e0046d958965f720855fa8785b78ac
SHUBHAM YADAV <raoshubham192@gmail.com>
2026-05-20 23:01:12 +0530

    Fix mixed content HTTPS issue for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'spa.auth' => \App\Http\Middleware\SpaAuthenticate::class,
         ]);
